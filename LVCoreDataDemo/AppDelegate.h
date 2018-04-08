236a7e633c6a6b1e1d085ed7d2b0ae65377ee6d0
nLoser <954777574@qq.com>
2018-04-08 10:55:00 +0800

    Initial Commit

---
+++ LVCoreDataDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LVCoreDataDemo
+//
+//  Created by meipai_lv on 2018/4/8.
+//  Copyright © 2018年 Meipai_Lv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
